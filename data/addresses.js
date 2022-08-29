1e0820c6f3f3cd4769892f2bf8f28736689d5cc7
haidaralimasu <haidaralimasu123@gmail.com>
2022-08-29 23:28:34 +0530

    minor changes

--- data/addresses.js
+++ data/addresses.js
@@ -1,11 +1,12 @@
 const whitelistAddresses = [
-  "0xA9889aAC1c8d0e8d5F874CdC9D475D0Dfcf32EB1",
-  "0xDf9f0593449Ec6e6a3706EBB505794FdB83FEF77",
-  "0xF9eF71BF3F5834BB75C9DD20E041bDfEbE931fD2",
-];
+  '0xA9889aAC1c8d0e8d5F874CdC9D475D0Dfcf32EB1',
+  '0xDf9f0593449Ec6e6a3706EBB505794FdB83FEF77',
+  '0xd50FAFBC4B77b49258CE7E964eBC71E8D01Fe355',
+  '0xF9eF71BF3F5834BB75C9DD20E041bDfEbE931fD2',
+]
 
 const getAddresses = () => {
-  return whitelistAddresses;
-};
+  return whitelistAddresses
+}
 
-module.exports = { getAddresses };
+module.exports = { getAddresses }
